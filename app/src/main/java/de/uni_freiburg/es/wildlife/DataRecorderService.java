2a162d22164f260efb0cc9bb11cc91b94bfd926a
Philipp M. Scholl <pscholl@ese.uni-freiburg.de>
2014-12-29 03:00:04 +0100

    support for FLAC file encoding

--- app/src/main/java/de/uni_freiburg/es/wildlife/DataRecorderService.java
+++ app/src/main/java/de/uni_freiburg/es/wildlife/DataRecorderService.java
@@ -3,25 +3,27 @@ package de.uni_freiburg.es.wildlife;
 import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.PendingIntent;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.SystemClock;
 import android.preference.PreferenceManager;
-import android.provider.ContactsContract;
-import android.provider.Settings;
 import android.util.Log;
 
 import java.io.File;
-import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Set;
+
+import javaFlacEncoder.FLACEncoder;
+import javaFlacEncoder.FLACFileOutputStream;
+import javaFlacEncoder.StreamConfiguration;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -52,6 +54,8 @@ public class DataRecorderService extends IntentService {
                     key.equals(SettingsFragment.PREF_STORAGE_PATH) ||
                     key.equals(SettingsFragment.PREF_SLEEP_TIME) ||
                     key.equals(SettingsFragment.PREF_WINDOW_SIZE)) {
+                /* this will restart an ongoing recording session or start a new one, if any
+                 * of the above listed settings has changed. */
                 myRecorderService.mIsRecording = false;
                 startActionRecord(myRecorderService);
             }
@@ -86,8 +90,21 @@ public class DataRecorderService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         if (intent != null) {
             final String action = intent.getAction();
-            if (ACTION_RECORD.equals(action))
-                handleActionRecord();
+            if (ACTION_RECORD.equals(action)) {
+                boolean isRecordingEnabled = mPreferences.getBoolean(
+                        SettingsFragment.PREF_RECORD_ON,
+                        SettingsFragment.PREF_RECORD_ON_DEFAULT);
+
+                if (!isRecordingEnabled)
+                {
+                    AlarmManager aman = (AlarmManager) getSystemService(ALARM_SERVICE);
+                    aman.cancel(mNextSchedule);
+                    mPreferences.edit().putBoolean(IS_RECORDING, false).commit();
+                    return;
+                }
+
+                handleActionRecordAudio();
+            }
         }
     }
 
@@ -102,83 +119,117 @@ public class DataRecorderService extends IntentService {
         mPreferences.registerOnSharedPreferenceChangeListener(MyChangeListener.newInstance(this));
     }
 
-    private void handleActionRecord() {
+    private void handleActionRecordAudio() {
         File storagePath = new File(mPreferences.getString(
                 SettingsFragment.PREF_STORAGE_PATH,
                 SettingsFragment.PREF_STORAGE_PATH_DEFAULT));
-        String filename = new File(storagePath, mDateFormat.format(new Date())+".aac").toString();
+        String filename = new File(storagePath,
+                mDateFormat.format(new Date())+".flac").toString();
         long sample_time = (long) mPreferences.getInt(
                 SettingsFragment.PREF_WINDOW_SIZE,
                 SettingsFragment.PREF_WINDOW_SIZE_DEFAULT) * 1000, // from seconds to millis
              sleep_time = (long) mPreferences.getInt(
-                 SettingsFragment.PREF_SLEEP_TIME,
-                 SettingsFragment.PREF_SLEEP_TIME_DEFAULT) * 1000;
+                SettingsFragment.PREF_SLEEP_TIME,
+                SettingsFragment.PREF_SLEEP_TIME_DEFAULT) * 1000;
 
         Log.d(TAG, "starting ");
-
-        boolean isRecordingEnabled = mPreferences.getBoolean(
-                SettingsFragment.PREF_RECORD_ON,
-                SettingsFragment.PREF_RECORD_ON_DEFAULT);
-
-        if (!isRecordingEnabled)
-        {
-            AlarmManager aman = (AlarmManager) getSystemService(ALARM_SERVICE);
-            aman.cancel(mNextSchedule);
-            mPreferences.edit().putBoolean(IS_RECORDING, false).commit();
-            return;
-        }
 
         if (!storagePath.canWrite())
             throw new UnsupportedOperationException(
                     "storage path is not writable: " + storagePath.toString());
 
-        /* prepare the recorder */
-        MediaRecorder rec = new MediaRecorder();
-        rec.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
-        rec.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS);
-        rec.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-        rec.setAudioSamplingRate(getMaxSampleRate());
-        rec.setOutputFile(filename);
+        /* do the recording */
+        doAudioRecording(filename, sample_time);
 
-        Log.e(TAG, "writing file to: " + filename);
-
-        try { /* do the recording */
-            rec.prepare();
-            rec.start();
-            mPreferences.edit().putBoolean(IS_RECORDING, true).commit();
-
-            mIsRecording = true;
-            Date beginning = new Date();
-            while (mIsRecording &&
-                   new Date().getTime() - beginning.getTime() < sample_time)
-                Thread.sleep(500);
-
-            rec.stop();
-            rec.release();
-            rec = null;
-        } catch (IOException e) {
-            Log.e(TAG, "prepare() failed: ", e);
-        } catch (InterruptedException e) {
-            Log.e(TAG, "sleep disturbed: ", e);
-        }
-
-        Log.e(TAG, "recording finished");
-        mPreferences.edit().putBoolean(IS_RECORDING, false).commit();
-
-        isRecordingEnabled = mPreferences.getBoolean(
-                SettingsFragment.PREF_RECORD_ON,
-                SettingsFragment.PREF_RECORD_ON_DEFAULT);
-
-        /* schedule repetition */
-        if (isRecordingEnabled)
-        {
-            AlarmManager aman = (AlarmManager) getSystemService(ALARM_SERVICE);
-            aman.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                     SystemClock.elapsedRealtime() + sleep_time,
-                     mNextSchedule);
-        }
+        /* schedule repetition, this whole process will be stopped when the next Record
+         * Intent is triggered, and the recording is not enabled! */
+        AlarmManager aman = (AlarmManager) getSystemService(ALARM_SERVICE);
+        aman.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                 SystemClock.elapsedRealtime() + sleep_time,
+                 mNextSchedule);
 
         enableLowPowerMode();
+    }
+
+    private void setIsRecording(boolean state) {
+        mPreferences.edit().putBoolean(IS_RECORDING, state).commit();
+        mIsRecording = state;
+    }
+
+    private void doAudioRecording(String filename, long sample_time_in_ms) {
+        /* prepare the recorder */
+        int samplerate = getMaxSampleRate();
+        int BUFSIZE = AudioRecord.getMinBufferSize(samplerate,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+        int flacbuf[] = new int[BUFSIZE/2];
+        short recbuf[] = new short[BUFSIZE/2];
+
+        AudioRecord rec = new AudioRecord(
+                MediaRecorder.AudioSource.CAMCORDER, samplerate,
+                AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,
+                BUFSIZE);
+
+        StreamConfiguration flac = new StreamConfiguration();
+        flac.setChannelCount(1);
+        flac.setBitsPerSample(16);
+        flac.setSampleRate(samplerate);
+
+        try {
+            for (Date beg = new Date();
+                 new Date().getTime() - beg.getTime() < 2000 &&
+                       rec.getState()!=rec.STATE_INITIALIZED;
+                 Thread.sleep(100))
+                ; // wait for init
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        setIsRecording(true);
+        rec.startRecording();
+
+        Log.e(TAG, "writing file to: " + filename);
+        try {
+            FLACEncoder fec = new FLACEncoder();
+            FLACFileOutputStream fos = new FLACFileOutputStream(filename);
+            fec.setStreamConfiguration(flac);
+            fec.setOutputStream(fos);
+            fec.openFLACStream();
+
+            Date beg = new Date();
+
+            while (mIsRecording && (new Date().getTime() - beg.getTime()) < sample_time_in_ms)
+            {
+                // additionally stop recording when mIsRecording changes to false, but only
+                // after a full block has been encoded,
+                int len = rec.read(recbuf, 0, recbuf.length);
+                if (len < 0) {
+                    Log.e(TAG, "failed to read from recorder: " + len);
+                    break;
+                }
+
+                // transfer to int buffer
+                for (int i=0; i<len; i++)
+                    flacbuf[i] = recbuf[i];
+
+                fec.addSamples(flacbuf, len);
+
+                while (fec.fullBlockSamplesAvailableToEncode() > 0)
+                    fec.t_encodeSamples(fec.fullBlockSamplesAvailableToEncode(), false);
+            }
+
+            fec.encodeSamples(0, true);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "file not found: ", e);
+        } catch (IOException e) {
+            Log.e(TAG, "io problem: ", e);
+        }
+
+        setIsRecording(false);
+        rec.stop();
+        rec = null;
+        Log.e(TAG, "recording finished");
     }
 
     private void enableLowPowerMode() {
